657d3decd9c0b1d0a136612de8fd8465106682a5
KumarBCharan <kumarbcharan143@gmail.com>
2024-09-17 23:17:28 +0530

    Update EMAIL.py

--- REGULAR eXPRESSIONS/EMAIL.py
+++ REGULAR eXPRESSIONS/EMAIL.py
@@ -4,3 +4,19 @@ mpo=re.findall('[a-zA-Z0-9]\w*[.]?\w+@gmail[.]com',s)
 for i in mpo:
     print(i)
 #print(isEmail("kumarbcharan77@gmail.com bvufh vj@gamil.c
+
+
+'''
+
+
+def isEmail(s):
+    import re
+    mpo=re.findall('[a-zA-Z1-9]\w*[.]?\w+@gmail[.]com',s)
+    for i in mpo:
+        print(i)
+
+    
+print(isEmail("kumar@gmail.comjhgf76 asifaharshad@gmail.com 272jyababa.hjc@gmail.com #baba&hjc@gmail.com"
+))
+
+'''
