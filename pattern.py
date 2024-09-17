93c2dd3972452751fcc4c69f7c79ea1ecc7e198a
KumarBCharan <kumarbcharan143@gmail.com>
2024-09-17 23:16:50 +0530

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -29,22 +29,8 @@ for row in range(1,n+1):
         
         star-=1
         space+=2
-'''
 
 
-def isEmail(s):
-    import re
-    mpo=re.findall('[a-zA-Z1-9]\w*[.]?\w+@gmail[.]com',s)
-    for i in mpo:
-        print(i)
-
-    
-print(isEmail("kumar@gmail.comjhgf76 asifaharshad@gmail.com 272jyababa.hjc@gmail.com #baba&hjc@gmail.com"
-))
-
-
-
-'''
 
 
 
